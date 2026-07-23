5bba0ac32242e24ccaf18df795c8184207bc0758
lexxaxell212 <febryagung404@gmail.com>
2026-07-24 01:38:19 +0700

    initial-v0.0.0

--- api/index.php
+++ api/index.php
@@ -6,6 +6,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 require_once __DIR__ . '/config/db.php';
+require_once __DIR__ . '/helpers/response.php';
 
 $route = trim($_GET['route'] ?? '', '/');
 
